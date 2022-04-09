068fd7fb3b62e9a69f9746da830168917b8948d2
Nuno Santos <NunoSantos2018@hotmai.com>
2022-04-09 13:03:56 +0100

    Código Inicial

---
+++ Codigo/matdin.c
@@ -0,0 +1,49 @@
+#include "f.h"
+
+
+void libertaMat(char** p, int nLin){
+
+    int i;
+
+    for(i=0; i<nLin; i++)
+        free(p[i]);
+    free(p);
+}
+
+char** criaMat(int nLin, int nCol){
+    char **p = NULL;
+    int i, j;
+
+    p = malloc(sizeof(char*) * nLin);
+    if(p == NULL)
+        return NULL;
+
+    for(i=0; i<nLin; i++){
+        p[i] = malloc(sizeof(char) * nCol);
+        if(p[i] == NULL){
+            libertaMat(p, i-1);
+            return NULL;
+        }
+        for(j=0; j<nCol; j++)
+            p[i][j] = '_';
+    }
+    return p;
+}
+
+void setPos(char **p, int x, int y, char c){
+    p[x][y] = c;
+}
+
+char getPos(char **p, int x, int y){
+    return p[x][y];
+}
+
+void mostraMat(char **p, int nLin, int nCol){
+    int i,j;
+
+    for(i=0; i<nLin; i++){
+        for(j=0; j<nCol; j++)
+            printf("%c\t", p[i][j]);
+        putchar('\n');
+    }
+}
---
+++ Codigo/matdin.h
@@ -0,0 +1,26 @@
+// Trabalho Pratico Programacao - LEI
+// DEIS-ISEC 2021-2022
+
+#ifndef MATDIN_H
+#define MATDIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+// Liberta uma matriz dinâmica de caracteres com nLin linhas
+void libertaMat(char** p, int nLin);
+
+// Cria uma matriz dinâmica de caracteres  com nLin linhas e nCol colunas
+// Devolve endereço inicial da matriz
+char** criaMat(int nLin, int nCol);
+
+// Imprime o conteúdo de uma matriz dinâmica de caracteres
+void mostraMat(char **p, int nLin, int nCol);
+
+// Coloca o caracter c na posição (x, y) de uma matriz dinâmica de caracteres
+void setPos(char **p, int x, int y, char c);
+
+// Obtém o caracter armazenado na posição (x, y) de uma matriz de caracteres
+char getPos(char **p, int x, int y);
+
+#endif /*  
